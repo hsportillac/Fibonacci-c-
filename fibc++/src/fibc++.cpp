d5b6c2d405225ee663865a6abc66a161f1cf751e
hsportillac <43122628+hsportillac@users.noreply.github.com>
2018-09-22 21:32:32 -0500

    Update

--- fibc++/src/fibc++.cpp
+++ fibc++/src/fibc++.cpp
@@ -2,17 +2,70 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n = 48, t1 = 0, t2 = 1, tn = 0;
-    cout << t1 << "\n";
-    cout << t2 << "\n";
-    for (int i = 3; i <= n; ++i)
+void calcFibChar(int n){
+   char t1 = 0, t2 = 1, tn = 0; 
+   cout << int(t1) << "\n";
+   cout << int(t2) << "\n";
+   for (int i = 3; i <= n; ++i)
     {
         tn = t1 + t2;
         t1 = t2;
         t2 = tn;
-        cout << tn << "\n";
+        cout << int(tn) << "\n";
     }
-    return 0;
+
 }
+
+void calcFibShort(int n){
+   short int t1 = 0, t2 = 1, tn = 0; 
+   cout << int(t1) << "\n";
+   cout << int(t2) << "\n";
+   for (int i = 3; i <= n; ++i)
+    {
+        tn = t1 + t2;
+        t1 = t2;
+        t2 = tn;
+        cout << int(tn) << "\n";
+    }
+
+}
+
+void calcFibInt(int n){
+   int t1 = 0, t2 = 1, tn = 0; 
+   cout << int(t1) << "\n";
+   cout << int(t2) << "\n";
+   for (int i = 3; i <= n; ++i)
+    {
+        tn = t1 + t2;
+        t1 = t2;
+        t2 = tn;
+        cout << int(tn) << "\n";
+    }
+
+}
+
+void calcFibLLI(int n){
+   long long int t1 = 0, t2 = 1, tn = 0; 
+   cout << int(t1) << "\n";
+   cout << int(t2) << "\n";
+   for (int i = 3; i <= n; ++i)
+    {
+        tn = t1 + t2;
+        t1 = t2;
+        t2 = tn;
+        cout << (long long int)(tn) << "\n";
+    }
+
+}
+
+int main()
+{   // Char limite n = 12
+    calcFibChar(12);
+    // Short limite n = 24
+    calcFibShort(24);
+    // Int limite n = 47
+    calcFibInt(47);
+    // Long long int limite n = 93
+    calcFibLLI(93);
+}
+
